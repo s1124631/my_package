be434f7f7cca435ad2f62d38d0d3fc066660cdeb
s1124631 <s1124631@mail.yzu.edu.tw>
2024-04-18 17:08:29 +0800

    first commit

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -2,7 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return_LaunchDescription([
+    return LaunchDescription([
         Node(
             package = 'demo_nodes_py',
             executable = 'listener'
